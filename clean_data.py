5809c9f4d7a82acf89f949d33be80ad41360d0af
djok216 <valeriu.s.motroi@info.uaic.ro>
2019-12-24 16:52:06 +0200

    fix script

--- clean_data.py
+++ clean_data.py
@@ -3,13 +3,11 @@ import datetime
 
 import pandas as pd
 
+fields = ['Date', 'IUCR', 'Primary Type', 'Arrest', 'Location Description', 'Domestic', 'Beat', 'District', 'Ward', 'X Coordinate', 'Y Coordinate', 'Latitude', 'Longitude']
+data = pd.read_csv('Crimes_-_2001_to_present.csv', usecols=fields, skipinitialspace=True).fillna(0)
 
-data = pd.read_csv('Crimes_-_2001_to_present.csv').fillna(0)
-data = data[['Date', 'IUCR', 'Primary Type', 'Arrest', 'Location Description', 'Domestic', 'Beat', 'District', 'Ward', 'X Coordinate', 'Y Coordinate', 'Latitude', 'Longitude']]
-
-# data[data == False] = 0.0 
-data = data.applymap(lambda x: 0.0 if x == False else x)
-data = data.applymap(lambda x: 1.0 if x == True else x)
+data = data.replace(True, 1.0)
+data = data.replace(False, 0.0)
 
 def normalize(data, field_name):
     ufield = set(data[field_name])
